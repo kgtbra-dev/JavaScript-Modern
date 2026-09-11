d9efb02a929ec1e1b42ca75193fb02093b4cf357
kgtbra-dev <kgtbra@gmail.com>
2026-09-11 18:58:20 -0300

    feat: adicionei o exercicio 18

---
+++ ex18.js
@@ -0,0 +1,10 @@
+const verificarIdade = (idade) => {
+    if (idade >= 18) {
+        return `Maior de idade` 
+    } else {
+        return `Menor de idade`
+    };
+};
+
+console.log(verificarIdade(17));
+
